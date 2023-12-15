e5bf2860384c01197da6b07b008db310d9fa1ba9
Tiirnako <treeleaves229@gmail.com>
2023-12-14 19:47:29 -0600

    Update tailwind.config.js
    Removed usage of slinky theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['luxury', 'aqua'],
+    themes: ['luxury', 'aqua', {
+      slinky: {
+
+        "primary": "#047857",
+
+        "secondary": '#3b82f6',
+
+        "accent": "#10b981",
+
+        "neutral": "#9ca3af",
+
+        "base-100": "#4b5563",
+
+        "info": "#ffffff",
+
+        "success": "#4ade80",
+
+        "warning": "#fcd34d",
+
+        "error": "#fb7185",
+      },
+    }],
     darkTheme: 'luxury',
   }
 };
